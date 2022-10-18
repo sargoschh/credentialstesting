34afeabb0f014022347cb86b012dcd8b6f079dd2
Claudio Landerer <landerer.claudio@gmail.com>
2022-10-18 15:04:38 +0200

    Start

---
+++ src/main/java/at/itkolleg/credentials-app/App.java
@@ -0,0 +1,13 @@
+package at.itkolleg.credentials-app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
