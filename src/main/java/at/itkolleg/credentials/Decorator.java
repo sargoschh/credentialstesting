cadd8882cd188ad2f4a8c0be43ab086e4d4620ee
sargosch <sargosch@tsn.at>
2022-11-16 09:22:50 +0100

    Dokumentation abgeschlossen

--- src/main/java/at/itkolleg/credentials/Decorator.java
+++ src/main/java/at/itkolleg/credentials/Decorator.java
@@ -2,18 +2,31 @@ package at.itkolleg.credentials;
 
 import java.util.List;
 
+/**
+ * Die Klasse Decorator fungiert als Mutterklasse für alle Schlussendlichen Decorator.
+ * Sie implementiert das Interface ExportCredentials und erhält ein Datenfeld
+ * vom Typ ExportCredentials, welches dann "dekoriert" wird.
+ * @author Sarah Gosch
+ * @version 1.0
+ */
 public abstract class Decorator implements ExportCredentials{
 
     private ExportCredentials csvExporter;
 
+    /**
+     * Im Konstruktor wird dem Decorator ein bereits erstellter Exporter mitgegeben
+     * (dieser wird dann dekoriert) und dem Datenfeld csvExporter zugewiesen.
+     * @param csvExporter - bereits erstellter CSVExporter vom Typ ExportCredentials
+     */
     public Decorator(ExportCredentials csvExporter) {
         this.csvExporter = csvExporter;
     }
 
-    public ExportCredentials getCsvExporter() {
-        return csvExporter;
-    }
-
+    /**
+     * Die Methode export() ruft lediglich auf den csvExporter (Datenfeld) die export() Methode
+     * aus der Klasse CsvExporter auf. Die "Dekoration" erfolgt dann in den Unterklassen.
+     * @param credentialsList - Liste mit Credentials
+     */
     @Override
     public void export(List<Credentials> credentialsList) {
         this.csvExporter.export(credentialsList);
--- src/main/java/at/itkolleg/credentials/HashExporter.java
+++ src/main/java/at/itkolleg/credentials/HashExporter.java
@@ -5,24 +5,54 @@ import com.google.common.hash.Hashing;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+/**
+ * Die Klasse HashExporter erbt vom Decorator. Diese erledigt dann das "Hashen" des Passworts
+ * und somit das Dekorieren des ursprünglichen Exports.
+ * @author Sarah Gosch
+ * @version 1.0
+ */
 public class HashExporter extends Decorator{
 
+    /**
+     * Dem Konstruktor des HashExporter wird ein bereits erstellter Exporter vom Typ
+     * ExportCredentials mitgegeben und mit super() an die Mutterklasse übergeben.
+     * Diese setzt damit das dort vorhandene Datenfeld csvExporter, welches nun im
+     * HashExporter dekoriert wird.
+     * @param csvExporter - bereits erstellter csvExporter vom Typ ExportCredentials
+     */
     public HashExporter(ExportCredentials csvExporter) {
         super(csvExporter);
     }
 
+    /**
+     * Die Methode export() dekoriert nun den Export der User-Login-Daten.
+     * Dazu wird eine neue Liste erstellt, die den Inhalt der übergebenen credentialsList
+     * erhält. Dies muss nicht so implementiert werden, da aber die ursprüngliche Liste im nächsten
+     * Schritt verändert wird und diese eventuell in der ursprünglichen Form noch benötigt wird,
+     * wird eine Kopie erstellt.
+     * Im nächsten Schritt wird die Liste mit einer ForEach Schleife durchlaufen, und das Passwort
+     * jeweils mit getHash() neu gesetzt.
+     * @param credentialsList - Liste mit Credentials
+     */
     @Override
     public void export(List<Credentials> credentialsList) {
         List<Credentials> newCredentialsList = credentialsList;
         for(Credentials c : newCredentialsList) {
-            c.setPwd(getHash(c));
+            c.setPwd(getHash(c.getPwd()));
         }
         super.export(newCredentialsList);
     }
 
-    public String getHash(Credentials credentials) {
+    /**
+     * Die Methode getHash() wandelt einen mitgegebenen String in einen Hash um.
+     * Dazu wird die Funktion Hashing verwendet. Es wird dann der Gehashte String
+     * zurückgegeben.
+     * @param string - String, der gehasht werden soll
+     * @return gehashter String
+     */
+    public String getHash(String string) {
         String sha256hex = Hashing.sha256()
-                .hashString(credentials.getPwd(), StandardCharsets.UTF_8)
+                .hashString(string, StandardCharsets.UTF_8)
                 .toString();
 
         return sha256hex;
